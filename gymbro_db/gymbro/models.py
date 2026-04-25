6f97d3063d9eab5187776cb66774e59b92a05685
oslosmok <adamtarach83@gmail.com>
2026-04-25 13:26:43 +0200

    added class users

--- gymbro_db/gymbro/models.py
+++ gymbro_db/gymbro/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class users(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.TextField(unique=True)
+    email = models.TextField(unique=True)
+    password = models.TextField()
+    first_name = models.TextField()
+    last_name = models.TextField()
+    created_at = models.DateTimeField()
