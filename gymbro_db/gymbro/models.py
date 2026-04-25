528a8cb96e9a20c215248f47a3d47989b8d15ec4
oslosmok <adamtarach83@gmail.com>
2026-04-25 13:42:23 +0200

    added class calendar_event

--- gymbro_db/gymbro/models.py
+++ gymbro_db/gymbro/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 
+# TO DO - dostosować enumy do potrzeb naszych tabel
+
+
+class EventTypes(models.choices):
+    WORKOUT = 'workout'
+    REST = 'rest'
+
 
 class users(models.Model):
-    id = models.AutoField(primary_key=True)
     username = models.TextField(unique=True)
     email = models.TextField(unique=True)
     password = models.TextField()
     first_name = models.TextField()
     last_name = models.TextField()
     created_at = models.DateTimeField()
+
+
+class calendar_events(models.Model):
+    user = models.ForeignKey('users', on_delete=models.CASCADE)
+    utc_time = models.DateTimeField()
+    event_type = models.CharField(max_length=50, choices=EventTypes.choices)
+    title = models.TextField()
+    description = models.TextField()
