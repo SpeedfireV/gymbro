7b20697a107ef5972af637cc144496d4d25fcb7b
oslosmok <adamtarach83@gmail.com>
2026-04-27 18:20:59 +0200

    added rest framework to settings

--- gymbro_db/gymbro_db/settings.py
+++ gymbro_db/gymbro_db/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gymbro.apps.GymbroConfig',
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        )
+}
