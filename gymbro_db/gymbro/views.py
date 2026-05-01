ee5252bb8f5f8c64a80fb2ccd79562ec2dafd112
oslosmok <adamtarach83@gmail.com>
2026-05-01 17:13:08 +0200

    fixed workout exercise add endpoint

--- gymbro_db/gymbro/views.py
+++ gymbro_db/gymbro/views.py
@@ -62,7 +62,7 @@ class WorkoutExerciseAddView(APIView):
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
