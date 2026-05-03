cb2081bc4d6762d756e71f8509298990318cc382
oslosmok <adamtarach83@gmail.com>
2026-05-03 11:34:48 +0200

    added workout urls

--- gymbro_db/gymbro/urls.py
+++ gymbro_db/gymbro/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import RegisterView, LoginView, WorkoutExerciseAddView, WorkoutExerciseDeleteView
+from .views import WorkoutAddView, WorkoutDeleteView
 
 
 urlpatterns = [
@@ -7,4 +8,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('workout-exercises/', WorkoutExerciseAddView.as_view(), name='add-workout-exercise'),
     path('workout-exercises/<int:pk>/', WorkoutExerciseDeleteView.as_view(), name='delete-workout-exercise'),
+    path('workouts/', WorkoutAddView.as_view(), name='add-workout'),
+    path('workouts/<int:pk>/', WorkoutDeleteView.as_view(), name='delete-workout'),
 ]
