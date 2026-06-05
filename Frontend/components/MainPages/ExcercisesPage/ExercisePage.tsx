0f5b82e5fcb1b9c52a07ade17a9fddd9d0272c5a
GNiesp <grzegorz.niespodzianek@gmail.com>
2026-06-05 21:23:14 +0200

    Exercise styles, image

--- Frontend/components/MainPages/ExcercisesPage/ExercisePage.tsx
+++ Frontend/components/MainPages/ExcercisesPage/ExercisePage.tsx
@@ -8,6 +8,8 @@ import ExerciseDescription from "./components/ExerciseDescription";
 import Spacer from "../../ReusableComponents/Spacer";
 import ExerciseBottomBar from "./components/ExerciseBottomBar";
 import { Exercise } from "../../ReusableComponents/ComplexTypes";
+import { ExerciseStylesProvider, styles } from './components/ExerciseStyles'
+
 
 
 export function ExercisePage({ navigation }: StackScreenProps<RootStackParamList, 'Exercise'>) {
@@ -24,89 +26,16 @@ export function ExercisePage({ navigation }: StackScreenProps<RootStackParamList
     }
   
   return (
-    <View style={styles.container}>
-      <ExerciseTopBar exerciseName={exercise.name} />
-      <ExerciseImageCarousel />
-      <ExerciseBodyParts bodyParts={exercise.muscle} />
-      <ExerciseDescription description={exercise.instructions} />
-      <Spacer />
-      <ExerciseBottomBar />
-    </View >
+    <ExerciseStylesProvider>
+      <View style={styles.container}>
+        <ExerciseTopBar exerciseName={exercise.name} />
+        <ExerciseImageCarousel />
+        <ExerciseBodyParts bodyParts={exercise.muscle} />
+        <ExerciseDescription description={exercise.instructions} />
+        <Spacer />
+        <ExerciseBottomBar />
+      </View >
+    </ExerciseStylesProvider>
   )
 }
 
-
-export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000000',
-  },
-  headerRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginHorizontal: 24,
-    marginTop: 48,
-    marginBottom: 24,
-  },
-  headerLeft: {
-    flexDirection: 'row',
-  },
-  exerciseTitle: {
-    fontFamily: 'BigShoulders-ExtraBold',
-    fontSize: 36,
-    color: '#FFFFFF',
-    marginLeft: 16,
-  },
-  imageRow: {
-    marginBottom: 24,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginHorizontal: 24,
-  },
-  metaRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginHorizontal: 24,
-    marginBottom: 24,
-  },
-  metaLeft: {
-    flexDirection: 'row',
-  },
-  metaIcon: {
-    marginRight: 16,
-  },
-  metaLabel: {
-    fontFamily: 'ChakraPetch-Bold',
-    fontSize: 20,
-    color: '#FFFFFF',
-  },
-  metaValue: {
-    fontFamily: 'ChakraPetch-SemiBold',
-    fontSize: 20,
-    color: '#FFFFFF',
-  },
-  description: {
-    fontFamily: 'ChakraPetch-Regular',
-    fontSize: 16,
-    color: '#EFF1F3',
-    textAlign: 'center',
-    marginHorizontal: 24,
-  },
-  spacer: {
-    flex: 1,
-  },
-  footerRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignSelf: 'stretch',
-    alignContent: 'center',
-    alignItems: 'center',
-    height: 64,
-    marginBottom: 32,
-    marginHorizontal: 24,
-  },
-
-
-}
-
-);
